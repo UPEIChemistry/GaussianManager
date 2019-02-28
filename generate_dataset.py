4c45be42427fd2b88ed221cc61139fb01cf3bebc
Riley Jackson <rjjackson22@gmail.com>
2019-02-28 18:26:46 -0400

    refactored generate_dataset.py script

--- generate_dataset.py
+++ generate_dataset.py
@@ -3,12 +3,14 @@
 import argparse
 from GaussianManager.src import calculations, executive, exceptions, utils
 import os
+from typing import List
 
 def get_args():
 
     parser = argparse.ArgumentParser(description='Run standard calculation suite on provided directory of molecules')
     parser.add_argument('-i', '--input', help='The input directory containing xyz files to run gaus calcs on')
     parser.add_argument('-o', '--output', help='the experimental root dir where data is written to')
+    parser.add_argument('-m', '--multiplicity', default='-1 1')
     args = parser.parse_args()
 
     return args
@@ -24,12 +26,10 @@ def main():
     error_log_path = root_dir + 'error_log.txt'
     geom_dir = root_dir + 'geometries/'
 
-    method_0, method_1, method_2, method_3 = 'MP2', 'B3LYP', 'M06', 'G4'
-    basis_0, basis_1, basis_2, basis_3 = 'cc-pVDZ', 'aug-cc-pVDZ', 'aug-cc-pVTZ', 'aug-cc-pVQZ'
+    methods = ['MP2', 'B3LYP', 'M06', 'G4']
+    basis_sets = ['cc-pVDZ', 'aug-cc-pVDZ', 'aug-cc-pVTZ', 'aug-cc-pVQZ']
 
-    calc_list = [calculations.TsoptCalc(method_0, basis_0, goal='ts'),
-                 calculations.IrcCalc(method_0, basis_0, direction='reverse'),
-                 calculations.IrcCalc(method_0, basis_0, direction='forward')]
+    calc_list = get_calc_list(methods, basis_sets)
 
     for mol in mol_list:
 
@@ -41,9 +41,21 @@ def main():
             msg = e.args[0] + ' on mol {}'.format(os.path.basename(mol)[:-4])
             utils.log_error(error_log_path, msg)
             continue
-        else:
-            for m_path in ex.output_mol_filepaths:
-                utils.compile_geom(geom_dir, m_path)
+
+def get_calc_list(methods: List, basis_sets: List):
+    """12 calcs in total, in sets of 3. First set is (ts, irc_r, irc_f) the next three are
+        (qst3, irc_r, irc_f)"""
+
+    calc_list = [calculations.TsoptCalc(methods[0], basis_sets[0], goal='ts'),
+                 calculations.IrcCalc(methods[0], basis_sets[0], direction='reverse'),
+                 calculations.IrcCalc(methods[0], basis_sets[0], direction='forward')]
+
+    for method, basis in zip(methods[1:], basis_sets[1:]):
+        calc_list.append(calculations.TsoptCalc(method, basis, goal='qst3'))
+        calc_list.append(calculations.IrcCalc(method, basis, direction='reverse'))
+        calc_list.append(calculations.IrcCalc(method, basis, direction='forward'))
+
+    return calc_list
 
 if __name__ == "__main__":
 
